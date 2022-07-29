0a7d2e46b2d497d754699224b437330963a174b9
bijoyrouth <rauthbijoy@gmail.com>
2022-07-29 16:03:45 +0530

    stage

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ export default class App extends Component {
     return (
       <div>
         <NavBar/>
-        <News/>
+        <News
+        pageSize={5}
+        />
       </div>
     )
   }
--- src/component/NewsItem.js
+++ src/component/NewsItem.js
@@ -2,10 +2,9 @@ import React, { Component } from 'react'
 
 export default class NewsItem extends Component {
   render() {
-    let {title, description, imageUrl, } = this.props
+    let {title, description, imageUrl, newsUrl} = this.props
     return (
-      <div className='px-16'>
-      <div className='px-3 py-5'>
+      <div className='sm:px-2 px-1 py-3'>
         <div className="max-w-sm rounded overflow-hidden shadow-lg">
           <img className="w-full" src={imageUrl} alt="Sunset in the mountains" />
           <div className="px-6 py-4">
@@ -13,9 +12,14 @@ export default class NewsItem extends Component {
             <p className="text-gray-700 text-base">
               {description}
             </p>
+          <div className='mt-3'>
+              <a className="bg-blue-300 px-2 py-2 rounded transition hover:bg-blue-500 hover:underline shadow"
+                href={newsUrl} target="_blank">
+                More-Info
+              </a>
+            </div>
           </div>
         </div>
-      </div>
       </div>
     )
   }
