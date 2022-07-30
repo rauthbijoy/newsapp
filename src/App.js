b130a82195a241318325ec35ebfd95af298eea6b
bijoyrouth <rauthbijoy@gmail.com>
2022-07-30 08:46:07 +0530

    implemented categories

--- src/App.js
+++ src/App.js
@@ -5,22 +5,44 @@ import {
   BrowserRouter as Router,
   Routes,
   Route,
-  Link
 } from "react-router-dom";
 
 export default class App extends Component {
+
+  constructor(){
+    super();
+    console.log('this is the constructor from app.js')
+    this.state = {
+      pageSize: 4,
+      country: 'us'    //setting the state for country and pagesize
+    }
+  }
   render() {
     return (
       <div>
+        {/*have to provide a uniques key for everyone to jump from one category to another category if wrapped in a single <Routes> func otherwise can use multiple routes to work with.*/}
         <Router>
         <NavBar/>
-        <News
-        pageSize={5}
-        country='in'
-        category='general'
-        />
         <Routes>
-          <Route path="/" element={<News pageSize={5} country='in' category='general'/>}></Route>
+          <Route exact path="/" element={<News pageSize={this.state.pageSize} country={this.state.country} category='general'/>}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/business" element={<News pageSize={this.state.pageSize} country={this.state.country} category='business'/>}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/entertainment" element={<News pageSize={this.state.pageSize} country={this.state.country} category='entertainment'/>}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/health" element={<News pageSize={this.state.pageSize} country={this.state.country} category='health'/>}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/science" element={<News pageSize={this.state.pageSize} country={this.state.country} category='science'/>}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/sports" element={<News pageSize={this.state.pageSize} country={this.state.country} category='sports'/>}></Route>
+        </Routes>
+        <Routes>
+          <Route exact path="/technology" element={<News pageSize={this.state.pageSize} country={this.state.country} category='technology'/>}></Route>
         </Routes>
         </Router>
       </div>
--- src/component/NavBar.js
+++ src/component/NavBar.js
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import {Link} from 'react-router-dom';
 
 function NavBar() {
   const [show, setShow] = useState(false);
@@ -38,8 +39,8 @@ function NavBar() {
                     <button type="button" className="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" id="menu-button" aria-expanded="true" aria-haspopup="true" onClick={() => setDrop(!drop)}>
                       Category
                       {/* <!-- Heroicon name: solid/chevron-down --> */}
-                      <svg className="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+                      <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                        <path fillRule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clipRule="evenodd" />
                       </svg>
                     </button>
                   </div>
@@ -48,13 +49,12 @@ function NavBar() {
                   {drop && (<div className="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                     <div className="py-1" role="none">
                       {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
-                      <a href="/business" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Business</a>
-                      <a href="/entertainment" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Entertainment</a>
-                      <a href="/eeneral" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">General</a>
-                      <a href="/health" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Health</a>
-                      <a href="/science" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Science</a>
-                      <a href="/sports" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Sports</a>
-                      <a href="/technology" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Technology</a>
+                      <Link to="/business" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Business</Link>
+                      <Link to="/entertainment" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Entertainment</Link>
+                      <Link to="/health" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Health</Link>
+                      <Link to="/science" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Science</Link>
+                      <Link to="/sports" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Sports</Link>
+                      <Link to="/technology" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Technology</Link>
                     </div>
                   </div>)}
                 </div>
@@ -121,13 +121,12 @@ function NavBar() {
                 {drop && (<div className="origin-top-right right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1">
                   <div className="py-1" role="none">
                     {/* <!-- Active: "bg-gray-100 text-gray-900", Not Active: "text-gray-700" --> */}
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Business</a>
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Entertainment</a>
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">General</a>
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Health</a>
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Science</a>
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Sports</a>
-                    <a href="#" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Technology</a>
+                    <Link to="/business" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">Business</Link>
+                      <Link to="/entertainment" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-1">Entertainment</Link>
+                      <Link to="/health" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Health</Link>
+                      <Link to="/science" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Science</Link>
+                      <Link to="/sports" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Sports</Link>
+                      <Link to="/technology" className="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-2">Technology</Link>
                   </div>
                 </div>)}
               </div>
